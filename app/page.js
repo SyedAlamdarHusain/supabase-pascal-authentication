6a8d07a7757e89104ffcdfda54f467fcdc109869
Syed Husain <144152175+SyedAlamdarHusain@users.noreply.github.com>
2023-10-30 18:16:28 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,32 @@
-import AuthForm from './auth-form'
+import AuthForm from './auth-form';
+
+const headerStyle = {
+  fontSize: '3rem',
+  textAlign: 'center',
+  marginBottom: '1rem',
+};
+
+const descriptionStyle = {
+  fontSize: '1.5rem',
+  textAlign: 'center',
+};
 
 export default function Home() {
   return (
-    <div className="row">
-      <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
-        <p className="">
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile. Fast, simple, secure.
-        </p>
-      </div>
-      <div className="col-6 auth-widget">
-        <AuthForm />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="text-center">
+            <h1 style={headerStyle}>Drift Travel Management</h1>
+            <p style={descriptionStyle}>
+              Experience Travel Management App. Create a user profile and upload an avatar image. Fast, simple, secure.
+            </p>
+          </div>
+        </div>
+        <div className="col-md-6 auth-widget">
+          <AuthForm />
+        </div>
       </div>
     </div>
-  )
+  );
 }
