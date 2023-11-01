fdde348d66a457fff4ac1afeb8cdc76dad9a3a0d
Syed Husain <144152175+SyedAlamdarHusain@users.noreply.github.com>
2023-11-01 16:52:51 -0400

    Updated account-form.jsx to connect to database account table

--- app/account/account-form.jsx
+++ app/account/account-form.jsx
@@ -1,186 +1,83 @@
-
-
-// 'use client'
-// import { useCallback, useEffect, useState } from 'react'
-// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
-// export default function AccountForm({ session }) {
-//   const supabase = createClientComponentClient()
-//   const [loading, setLoading] = useState(true)
-//   const [fullname, setFullname] = useState(null)
-//   const [username, setUsername] = useState(null)
-//   const [website, setWebsite] = useState(null)
-//   const [avatar_url, setAvatarUrl] = useState(null)
-//   const user = session?.user
-
-//   const getProfile = useCallback(async () => {
-//     try {
-//       setLoading(true)
-
-//       const { data, error, status } = await supabase
-//         .from('profiles')
-//         .select(`full_name, username, website, avatar_url`)
-//         .eq('id', user?.id)
-//         .single()
-
-//       if (error && status !== 406) {
-//         throw error
-//       }
-
-//       if (data) {
-//         setFullname(data.full_name)
-//         setUsername(data.username)
-//         setWebsite(data.website)
-//         setAvatarUrl(data.avatar_url)
-//       }
-//     } catch (error) {
-//       alert('Error loading user data!')
-//     } finally {
-//       setLoading(false)
-//     }
-//   }, [user, supabase])
-
-//   useEffect(() => {
-//     getProfile()
-//   }, [user, getProfile])
-
-//   async function updateProfile({ username, website, avatar_url }) {
-//     try {
-//       setLoading(true)
-
-//       const { error } = await supabase.from('profiles').upsert({
-//         id: user?.id,
-//         full_name: fullname,
-//         username,
-//         website,
-//         avatar_url,
-//         updated_at: new Date().toISOString(),
-//       })
-//       if (error) throw error
-//       alert('Profile updated!')
-//     } catch (error) {
-//       alert('Error updating the data!')
-//     } finally {
-//       setLoading(false)
-//     }
-
-//   }
-
-//   // Apply inline styles for centering the form
-//   const centeredFormStyle = {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     height: '100vh',
-//   }
-  
-
-//   return (
-    
-//     <div className="form-widget">
-//       <div>
-//         <label htmlFor="email">Email </label>
-//         <input id="email" type="text" value={session?.user.email} disabled />
-//       </div>
-//       <div>
-//         <label htmlFor="fullName">Full Name </label>
-//         <input
-//           id="fullName"
-//           type="text"
-//           value={fullname || ''}
-//           onChange={(e) => setFullname(e.target.value)}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="ubername2">Username </label>
-//         <input
-//           id="username"
-//           type="text"
-//           value={username || ''}
-//           onChange={(e) => setUsername(e.target.value)}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="website">Website </label>
-//         <input
-//           id="website"
-//           type="url"
-//           value={website || ''}
-//           onChange={(e) => setWebsite(e.target.value)}
-//         />
-//       </div>
-
-//       <div>
-//         <button
-//           className="button primary block"
-//           onClick={() => updateProfile({ fullname, username, website, avatar_url })}
-//           disabled={loading}
-//         >
-//           {loading ? 'Loading ...' : 'Update'}
-//         </button>
-//       </div>
-
-//       <div>
-//         <form action="/auth/signout" method="post">
-//           <button className="button block" type="submit">
-//             Sign out
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
-
 'use client'
-import { useCallback, useEffect, useState } from 'react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
-import Avatar from './avatar'
+import { useCallback, useEffect, useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import axios from 'axios';
 
 export default function AccountForm({ session }) {
-  const supabase = createClientComponentClient()
-  const [loading, setLoading] = useState(true)
-  const [fullname, setFullname] = useState(null)
-  const [username, setUsername] = useState(null)
-  const [website, setWebsite] = useState(null)
-  const [avatar_url, setAvatarUrl] = useState(null)
-  const user = session?.user
+  const supabase = createClientComponentClient();
+  const [loading, setLoading] = useState(true);
+  const [fullname, setFullname] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [website, setWebsite] = useState(null);
+  const [avatar_url, setAvatarUrl] = useState(null);
+  const user = session?.user;
+  
+  const [userData, setUserData] = useState({
+    user_name: "",
+    first_name: "",
+    last_name: "",
+    password_hash: "",
+    location: "",
+    email: session?.user.email, // Include email from Supabase
+
+  });
+  const [accountMessage, setAccountMessage] = useState(""); // State for the account creation message
+
+  const apiUrl = 'http://127.0.0.1:5000/account';
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const createAccount = async () => {
+    try {
+      const response = await axios.post(apiUrl, userData);
+
+      if (response.status === 201) {
+        setAccountMessage("Account created successfully!"); // Set the account creation message
+      } else {
+        setAccountMessage(`Error: ${response.data.error}`);
+      }
+    } catch (error) {
+      setAccountMessage(`Error: ${error}`);
+    }
+  }
 
   const getProfile = useCallback(async () => {
     try {
-      setLoading(true)
+      setLoading(true);
 
       const { data, error, status } = await supabase
         .from('profiles')
         .select(`full_name, username, website, avatar_url`)
         .eq('id', user?.id)
-        .single()
+        .single();
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setFullname(data.full_name)
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setFullname(data.full_name);
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
-      alert('Error loading user data!')
+      alert('Error loading user data!');
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [user, supabase])
+  }, [user, supabase]);
 
   useEffect(() => {
-    getProfile()
-  }, [user, getProfile])
+    getProfile();
+  }, [user, getProfile]);
 
   async function updateProfile({ username, website, avatar_url }) {
     try {
-      setLoading(true)
+      setLoading(true);
 
       const { error } = await supabase.from('profiles').upsert({
         id: user?.id,
@@ -189,97 +86,98 @@ export default function AccountForm({ session }) {
         website,
         avatar_url,
         updated_at: new Date().toISOString(),
-      })
-      if (error) throw error
-      alert('Profile updated!')
+      });
+      if (error) throw error;
+      alert('Profile updated!');
     } catch (error) {
-      alert('Error updating the data!')
+      alert('Error updating the data!');
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
-  const centeredFormStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh', // Ensure the form takes up the entire viewport height
-  };
-
   return (
-      <div style={centeredFormStyle}>
-        <div className="form-widget">
-          {/* Add to the body */}
-          <Avatar
-            uid={user.id}
-            url={avatar_url}
-            size={150}
-            onUpload={(url) => {
-              setAvatarUrl(url)
-              updateProfile({ fullname, username, website, avatar_url: url })
-            }}
+    <div>
+      <h1>User Login</h1>
+      <div className="form-widget">
+        <div>
+          <label htmlFor="email">Email : </label>
+          <input
+            id="email"
+            type="text"
+            value={session?.user.email}
+            style={{ display: 'inline-block', width: '50%' }}
+            disabled
           />
-          <div>
-
-          </div>
+        </div>
 
 
-        <div className="form-widget">
-          <div>
-            <label htmlFor="email">Email </label>
-            <input id="email" type="text" value={session?.user.email} disabled />
-          </div>
-          <div>
-            <label htmlFor="fullName">Full Name </label>
-            <input
-              id="fullName"
-              type="text"
-              value={fullname || ''}
-              onChange={(e) => setFullname(e.target.value)}
-              style={{ color: 'black', background: 'lightgray' }} // Adjust text and background color
-            />
-          </div>
-          <div>
-            <label htmlFor="ubername2">Username </label>
-            <input
-              id="username"
-              type="text"
-              value={username || ''}
-              onChange={(e) => setUsername(e.target.value)}
-              style={{ color: 'black', background: 'lightgray' }} // Adjust text and background color
-            />
-          </div>
-          <div>
-            <label htmlFor="website">Website </label>
-            <input
-              id="website"
-              type="url"
-              value={website || ''}
-              onChange={(e) => setWebsite(e.target.value)}
-              style={{ color: 'black', background: 'lightgray' }} // Adjust text and background color
-            />
-          </div>
-
-          <div>
-            <button
-              className="button primary block"
-              onClick={() => updateProfile({ fullname, username, website, avatar_url })}
-              disabled={loading}
-            >
-              {loading ? 'Loading ...' : 'Update'}
+      {/* Your Account Creation Form */}
+      <form>
+        <div className="mb-3">
+          <label>User Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="user_name"
+            value={userData.user_name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>First Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="first_name"
+            value={userData.first_name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="last_name"
+            value={userData.last_name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password: </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password_hash"
+            value={userData.password_hash}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Location: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="location"
+            value={userData.location}
+            onChange={handleChange}
+          />
+        </div>
+        {accountMessage && <p>{accountMessage}</p>} {/* Display the account creation message */}
+        <button type="button" onClick={createAccount} className="btn btn-primary">
+          Create Account
+        </button>
+      </form>
+      <div>
+          <form action="/auth/signout" method="post">
+            <button className="button block" type="submit">
+              Sign out
             </button>
-          </div>
-
-          <div>
-            <form action="/auth/signout" method="post">
-              <button className="button block" type="submit">
-                Sign out
-              </button>
-            </form>
-          </div>
+          </form>
         </div>
       </div>
     </div>
-    );
+  );
+}
 
-  }
